2cb1d8ede4b21960ac8bec7e48052e2aacdf0f6a
freddietheodo <117086272+freddietheodo@users.noreply.github.com>
2023-01-10 17:10:29 +0000

    feat: added spinning pie

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import "./App.css";
 import Header from "./components/molecules/Header/Header";
 import Footer from "./components/molecules/Footer/Footer";
+import { Rotate } from "react-animated-components";
+
+const BoringPie = () => <span>🥧</span>;
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="app">
-        <h1>Edit Your Application Here</h1>
+    <div className="app">
+      <div className="app-header">
+        <Header />
       </div>
-      <Footer />
-    </>
+      <div className="app-body">
+        <h1>Welcome to the Theodo UK Hackathon!</h1>
+        <h2>This is the skeleton repository containing:</h2>
+        <ul>
+          <li>An example component (Header and Footer)</li>
+          <li>An example image (Theodo Logo)</li>
+          <li>An example button </li>
+          <li>An example text input: </li>
+        </ul>
+        <h3>
+          {" "}
+          An example of an animated pie:{" "}
+          <Rotate>
+            {" "}
+            <BoringPie />
+          </Rotate>
+        </h3>
+      </div>
+      <div className="app-footer">
+        <Footer />
+      </div>
+    </div>
   );
 }
 
